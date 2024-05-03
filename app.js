7cb1e0c38eb503d1b67824b13c35cd0d511b275a
Zach Whitsett <111324096+zachmvm@users.noreply.github.com>
2024-05-03 19:19:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ cron.schedule("0 0 * * *", () => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
